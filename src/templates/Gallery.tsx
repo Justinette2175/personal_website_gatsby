ec29bddf1ef3025b741b929161ca194630aa881c
justinette2175 <jugagnepain75@gmail.com>
2021-02-15 20:29:46 -0500

    changed yaml

---
+++ src/templates/Gallery.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+
+export default function Project({ data }) {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: post.frontmatter.description }}
+        />
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        description
+      }
+    }
+  }
+`
